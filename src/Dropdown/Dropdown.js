e205ed6d1ba8f75444fed27a5f94848aea8b6ec2
Jen Proctor <jen_p_13@hotmail.co.uk>
2020-02-07 16:27:52 +0000

    Handle dropdown and value clicks in Dropdown component

--- src/Dropdown/Dropdown.js
+++ src/Dropdown/Dropdown.js
@@ -2,6 +2,35 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 export default class Dropdown extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showList: false,
+      values: []
+    };
+
+    this.handleDropdownClick = this.handleDropdownClick.bind(this);
+    this.handleValueClick = this.handleValueClick.bind(this);
+  }
+
+  handleDropdownClick() {
+    this.setState(state => ({
+      showList: !state.showList
+    }));
+  }
+
+  handleValueClick(clickedItem) {
+    let newValues = this.state.values;
+
+    if (newValues.some(item => item.value === clickedItem.value)) {
+      newValues = newValues.filter(item => item.value !== clickedItem.value);
+    } else {
+      newValues.push(clickedItem);
+    }
+
+    this.setState({ values: newValues });
+  }
+
   render() {
     const options = this.props.data
       ? this.props.data.map((item, index) => {
@@ -12,24 +41,39 @@ export default class Dropdown extends Component {
         })
       : [{ label: "", value: 0 }];
 
-    console.log("OPTIONS: ", options);
+    console.log("VALUES: ", this.state.values);
 
     return (
       <div>
         <h3>Dropdown</h3>
 
         <div>
-          <p>{this.props.placeholder}</p>
+          <p onClick={this.handleDropdownClick}>
+            {this.state.values.length > 0
+              ? this.props.subtitle
+              : this.props.placeholder}
+          </p>
 
-          <ul>
-            {options.map(item => (
-              <li key={item.value}>
-                {item.value}: {item.label}
-              </li>
-            ))}
-          </ul>
+          {this.state.values.length > 0 && (
+            <ul>
+              {this.state.values.map(item => (
+                <li key={item.value}>{item.label}</li>
+              ))}
+            </ul>
+          )}
 
-          <p>{this.props.subtitle}</p>
+          {this.state.showList && (
+            <ul>
+              {options.map(item => (
+                <li
+                  key={item.value}
+                  onClick={() => this.handleValueClick(item)}
+                >
+                  {item.value}: {item.label}
+                </li>
+              ))}
+            </ul>
+          )}
         </div>
       </div>
     );
