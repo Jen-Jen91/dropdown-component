f2b881c3ca4f373e57bd93e9b3db3159c9d45026
Jen Proctor <jen_p_13@hotmail.co.uk>
2020-02-07 15:33:50 +0000

    Import PropTypes

--- src/Dropdown/Dropdown.js
+++ src/Dropdown/Dropdown.js
@@ -1,13 +1,43 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-class Dropdown extends Component {
+export default class Dropdown extends Component {
   render() {
+    const options = this.props.data
+      ? this.props.data.map((item, index) => {
+          return {
+            label: item,
+            value: index
+          };
+        })
+      : [{ label: "", value: 0 }];
+
+    console.log("OPTIONS: ", options);
+
     return (
       <div>
         <h3>Dropdown</h3>
+
+        <div>
+          <p>{this.props.placeholder}</p>
+
+          <ul>
+            {options.map(item => (
+              <li key={item.value}>
+                {item.value}: {item.label}
+              </li>
+            ))}
+          </ul>
+
+          <p>{this.props.subtitle}</p>
+        </div>
       </div>
     );
   }
 }
 
-export default Dropdown;
+Dropdown.propTypes = {
+  placeholder: PropTypes.string.isRequired,
+  subtitle: PropTypes.string.isRequired,
+  data: PropTypes.arrayOf(PropTypes.string).isRequired
+};
--- src/Main/Main.js
+++ src/Main/Main.js
@@ -4,10 +4,25 @@ import Dropdown from "../Dropdown/Dropdown";
 
 class Main extends Component {
   render() {
+    const mechanismData = [
+      "Abrasion",
+      "Blunt",
+      "Burn",
+      "Frost",
+      "Gun Shot",
+      "Laceration",
+      "RTC",
+      "Stab"
+    ];
+
     return (
       <div className="main">
         <h1>MAIN</h1>
-        <Dropdown />
+        <Dropdown
+          placeholder="Mechanism"
+          subtitle="Mechanism of Injury"
+          data={mechanismData}
+        />
       </div>
     );
   }
