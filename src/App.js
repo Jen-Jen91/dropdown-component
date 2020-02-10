88f20a59cc5ce996347bde993ba2229405ceb6a0
Jen Proctor <jen_p_13@hotmail.co.uk>
2020-02-10 10:48:56 +0000

    Install SASS and change css files to scss

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
 import Dropdown from "./Dropdown/Dropdown";
 
 function App() {
--- src/Dropdown/Dropdown.js
+++ src/Dropdown/Dropdown.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import "./Dropdown.css";
+import "./Dropdown.scss";
 
 export default class Dropdown extends Component {
   constructor(props) {
