6ef1071269d108512187ba6fb6e4282b72a851b9
Jen Proctor <jen_p_13@hotmail.co.uk>
2020-02-10 10:21:45 +0000

    Delete unnecessary Main component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from "react";
-import Main from "./Main/Main";
+import "./App.css";
+import Dropdown from "./Dropdown/Dropdown";
 
 function App() {
-  return <Main />;
+  const mechanismData = [
+    "Abrasion",
+    "Blunt",
+    "Burn",
+    "Frost",
+    "Gun Shot",
+    "Laceration",
+    "RTC",
+    "Stab"
+  ];
+
+  return (
+    <div className="app">
+      <Dropdown
+        placeholder="Mechanism"
+        subtitle="Mechanism of Injury"
+        data={mechanismData}
+      />
+    </div>
+  );
 }
 
 export default App;
